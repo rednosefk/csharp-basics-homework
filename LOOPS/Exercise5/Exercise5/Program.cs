57c73660e1413626cfce690fd63c2f70970aed48
Vladislavs Germanovics <vladislavsgermanovich@gmail.com>
2019-08-19 20:01:23 +0300

    added loops

---
+++ LOOPS/Exercise5/Exercise5/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstWord, secondWord, dots, result;
+            int count, period, i;
+
+            Console.Write("Enter a word #1: ");
+            firstWord = Console.ReadLine();
+            Console.Write("Enter a word #2: ");
+            secondWord = Console.ReadLine();
+
+            result = firstWord + "" + secondWord;
+            count = result.Length;
+            period = 0;
+
+            if (count <= 30)
+            {
+                period = 30 - count;
+                dots = "";
+
+                for (i = 1; i <= period; i++)
+                {
+                    dots += ".";
+                }
+
+                Console.WriteLine(firstWord + dots + secondWord);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ LOOPS/Exercise6/Exercise6/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+
+
+            for (i = 1; i <= 110; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    Console.Write(" Fizz ");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.Write(" Buzz ");
+                }
+                
+                else if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.Write(" FizzBuzz ");
+                }
+                else
+                {
+                    Console.Write(" " + i + " ");
+                }
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
